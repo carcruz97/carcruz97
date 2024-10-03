e6b055583ab1024530183259f0364a2843c5da63
Carmen Cruzado <95239086+carcruz97@users.noreply.github.com>
2024-10-03 00:07:45 -0300

    Update ProfileCard.tsx

--- app/components/ProfileCard.tsx
+++ app/components/ProfileCard.tsx
@@ -1,1 +1,24 @@
+import Image from 'next/image'
+import { cvData } from '../services/cvData'
+
+const ProfileCard = ({ language }: { language: 'en' | 'es' }) => (
+  <div className="profile-card">
+    <div className="profile-image-container">
+      <Image
+        src="/perfil.png"
+        alt="Profile Picture"
+        width={100}
+        height={100}
+        className="rounded-full border-2 border-white"
+        unoptimized
+      />
+    </div>
+    <div className="profile-info text-center">
+      <h1 className="profile-name">{cvData[language].name}</h1>
+      <p className="profile-title">{cvData[language].title}</p>
+    </div>
+  </div>
+)
+
+export default ProfileCard
 
