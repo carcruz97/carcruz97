001869b5cebf9ab7e0778d52dab1296655cfa534
Carmen Cruzado <95239086+carcruz97@users.noreply.github.com>
2024-10-03 18:51:40 -0300

    Delete app directory

--- app/generatePDF.ts
+++
@@ -1,9 +0,0 @@
-import html2pdf from 'html2pdf.js'
-
-export const generatePDF = (cvRef: React.RefObject<HTMLDivElement>, language: 'en' | 'es') => {
-  if (cvRef.current) {
-    html2pdf()
-      .from(cvRef.current)
-      .save(`cv_${language}.pdf`)
-  }
-}
