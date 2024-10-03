6475a72fcfc26354a3c227cf9e0ed3e7cf6db8f1
Carmen Cruzado <95239086+carcruz97@users.noreply.github.com>
2024-10-03 13:15:02 -0300

    Update commandHandlers.ts

--- app/services/commandHandlers.ts
+++ app/services/commandHandlers.ts
@@ -1,28 +1,24 @@
-export const commandData = {
-  about: {
-    en: { command: 'about', output: 'I am Carmen Cruzado, a Machine Learning Engineer with a passion for AI.' },
-    es: { command: 'sobre mí', output: 'Soy Carmen Cruzado, una ingeniera de aprendizaje automático apasionada por la IA.' }
-  },
-  stack: {
-    en: { command: 'stack', output: 'Python, TensorFlow, PyTorch, Kubernetes, Docker.' },
-    es: { command: 'stack', output: 'Python, TensorFlow, PyTorch, Kubernetes, Docker.' }
-  }
-  // Add more commands as necessary
-}
+import { commands, commandRegexes } from './commandData'
 
-export const findCommand = (input: string, language: 'en' | 'es') => {
-  const lowerInput = input.trim().toLowerCase()
-
-  if (lowerInput === 'clear') {
-    return 'clear'
-  }
-
-  for (const command in commandData) {
-    if (commandData[command][language].command === lowerInput) {
+export const findCommand = (input: string): string | null => {
+  for (const [command, regex] of Object.entries(commandRegexes)) {
+    if (regex.test(input)) {
       return command
     }
   }
-
   return null
 }
 
+export const handleCommand = (input: string, language: 'en' | 'es'): string[] => {
+  const trimmedInput = input.trim().toLowerCase()
+  const matchedCommand = findCommand(trimmedInput)
+
+  if (matchedCommand === 'clear') {
+    return []
+  } else if (matchedCommand) {
+    const commandOutput = commands[matchedCommand as keyof typeof commands][language]
+    return [`$ ${commandOutput.command}`, commandOutput.output]
+  } else {
+    return [`$ ${input}`, language === 'en' ? `Command not found: ${input}. Type a valid command to explore the portfolio.` : `Comando no encontrado: ${input}. Escribe un comando válido para explorar el portafolio.`]
+  }
+}
