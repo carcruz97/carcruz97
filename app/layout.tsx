70e2b45b0b757031e16ca6042ef24a95ddc0a03f
Carmen Cruzado <95239086+carcruz97@users.noreply.github.com>
2024-10-04 17:27:23 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import 'globals.css'
+import './globals.css'; // If globals.css is in the same directory as layout.tsx
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
